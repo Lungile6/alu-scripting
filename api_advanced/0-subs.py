fc42a2b75c4298b5e2eb2ecb5cb039c389e5042b
Lungile6 <155407787+Lungile6@users.noreply.github.com>
2024-07-06 22:23:48 +0200

    Update 0-subs.py
    Fixing 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-"""Return number of subscribers."""
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-
-    url = f"http://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    r = requests.get(
+        'http://www.reddit.com/r/{}/about.json'.format(subreddit),
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                        "AppleWebKit/537.36 (KHTML, like Gecko) "
-                       "Chrome/126.0.0.0 Safari/537.36")
-    }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get("data", {}).get("subscribers", 0)
-    except requests.RequestException:
-        return 0
-    return 0
+                       "Chrome/126.0.0.0 Safari/537.36"
+        }).json()
+    subscribers = r.get("data", {}).get("subscribers", 0)
+    return subscribers
+    
