a1df7f1937908682a212f25f7faa15af2335ce88
Lungile6 <155407787+Lungile6@users.noreply.github.com>
2024-07-06 22:35:41 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,42 +1,30 @@
 #!/usr/bin/python3
-"""Returning hot top ten."""
+"""
+Contains the top_ten function
+"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the top ten hot posts for a given subreddit."""
+    """prints the titles of the top ten hot posts for a given subreddit"""
     if subreddit is None or not isinstance(subreddit, str):
         print(None)
-        return
-
-    url = f"http://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {
-        "User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                       "AppleWebKit/537.36 (KHTML, like Gecko) "
-                       "Chrome/126.0.0.0 Safari/537.36")
-    }
-    params = {'limit': 10}
-
-    try:
-        response = requests.get(
-            url, headers=headers, params=params, allow_redirects=False)
-
-        # Check if the request was successful
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-            if not posts:
-                print(None)
-                return
-
-            for post in posts:
-                print(post.get('data', {}).get('title', None))
-        else:
-            print(None)
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
+    r = requests.get(
+        'http://www.reddit.com/r/{}/hot.json'.format(subreddit),
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/126.0.0.0 Safari/537.36"
+        },
+        params={
+            'limit': 10
+        },
+        allow_redirects=False
+    ).json()
+    posts = r.get('data', {}).get('children', None)
+    if posts is None or (len(posts) > 0 and posts[0].get('kind') != 't3'):
         print(None)
-    except ValueError:
-        print("Invalid JSON response")
-        print(None)
+    else:
+        for post in posts:
+            print(post.get('data', {}).get('title', None))
