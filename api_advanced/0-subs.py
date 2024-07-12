a1074a20a257e4b668bc906b3ceddbff388e4261
Lungile6 <155407787+Lungile6@users.noreply.github.com>
2024-07-12 12:43:48 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -11,9 +11,9 @@ def number_of_subscribers(subreddit):
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = \
-        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
-          AppleWebKit/537.36(KHTML, like Gecko) \
-         Chrome/90.0.4430.93 Safari/537.36'}
+        {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                        "AppleWebKit/537.36 (KHTML, like Gecko) "
+                        "Chrome/126.0.0.0 Safari/537.36"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
